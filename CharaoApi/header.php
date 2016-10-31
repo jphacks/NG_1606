dbd6d61b39c20517a22ea13361a67d321ecb864d
Ryosuke ONOSE <dev@onose.ws>
2016-11-01 00:23:31 +0900

    init template theme

---
+++ CharaoApi/header.php
@@ -0,0 +1,7 @@
+<html>
+<head>
+<title>Charao Api</title>
+<link media="screen" rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+</head>
+<body>
