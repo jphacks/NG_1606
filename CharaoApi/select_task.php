3f6a4352d4e3f00aff2e2c01af24914bdbb5455a
Ryosuke ONOSE <dev@onose.ws>
2016-11-05 00:20:04 +0900

    タスクプールからランダムでタスクを表示

--- CharaoApi/select_task.php
+++ CharaoApi/select_task.php
@@ -1,11 +1,25 @@
 <?php /* Template Name: select_task */
 get_header(); ?>
 
-Hello World
+<ul id="task-list">
+</ul>
 
 <script>
   $(function(){
     console.log("jQuery move");
+  });
+  $(document).ready(function(){
+    $.getJSON("<?php echo home_url('/');?>wp-json/wp/v2/task?filter[orderby]=rand&_embed&filter[nopaging]=true", function(data){
+      console.log(data);
+      for(var i in data){
+        console.log(data[i].title.rendered);
+        $(data[i]._embedded['wp:featuredmedia']).each(function(index, element){
+          media_url = element.source_url;
+        });
+        console.log(media_url);
+        $('#task-list').append('<li><img src="' + media_url + '" width="100"> ' + data[i].title.rendered + '</li>');
+      }
+    });
   });
 </script>
 
