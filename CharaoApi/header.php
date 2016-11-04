4cabe73d29a570c777dd4f7f8f152d0a70fc3b62
Kota <kota.github@gmail.com>
2016-11-04 13:52:17 +0900

    add jQuery CDN

--- CharaoApi/header.php
+++ CharaoApi/header.php
@@ -2,6 +2,7 @@
 <head>
 <title>Charao Api</title>
 <link media="screen" rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css">
+<script  src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
 <meta name="viewport" content="width=device-width,initial-scale=1">
 </head>
 <body>
