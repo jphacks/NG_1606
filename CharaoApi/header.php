3cde02ad3494895a2037b188899a03f4dfb3ac3c
Ryosuke ONOSE <dev@onose.ws>
2016-11-04 18:39:10 +0900

    add tasks into README

--- CharaoApi/header.php
+++ CharaoApi/header.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 <title>Charao Api</title>
+<link media="screen" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css">
 <link media="screen" rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css">
 <meta name="viewport" content="width=device-width,initial-scale=1">
 </head>
