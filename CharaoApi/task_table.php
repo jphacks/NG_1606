5fc6910b774253e90fa9ed1d0117e8f9ef4c0053
Ryosuke ONOSE <dev@onose.ws>
2016-11-06 01:47:06 +0900

    script を整理

--- CharaoApi/task_table.php
+++ CharaoApi/task_table.php
@@ -1,6 +1,14 @@
 <?php /* Template Name: task_table */
 get_header(); ?>
 <div class="select-task-page">
+<div class="btn-group btn-group-justified" role="group" aria-label="...">
+  <div class="btn-group" role="group">
+    <a href="javascript:void(0)" class="btn btn-default" onclick="hide_done_task()">未完了</a>
+  </div>
+  <div class="btn-group" role="group">
+    <a href="javascript:void(0)" class="btn btn-default" onclick="show_done_task()">すべて</a>
+  </div>
+</div>
   <!--
   <p>
     <a href="javascript:void(0)" class="btn btn-default btn-reload-task">タスクプールを更新</a>
@@ -11,57 +19,23 @@ get_header(); ?>
   </table>
 </div>
 
+
+<?php include 'script.php'; ?>
 <script>
+function hide_done_task(){
+  $('*[data-task_checked="true"]').hide();
+  load_task_table($('#task-table'));
+}
+function show_done_task(){
+  $('*[data-task_checked="true"]').show();
+  load_task_table($('#task-table'));
+}
 
-  function load_task_table(table){
-    table.empty();
-    path = "<?php echo home_url('');?>" + '/wp-json/wp/v2/my_task?_embed';
-    $.ajax({type: 'GET', url: path, dataType: 'json'}).done(function(json, textStatus, request){
-      page_amount = request.getResponseHeader('X-WP-TotalPages');
-      page_amount = +page_amount; // キャスト
-      console.log(page_amount);
-      for(var p = 1; p < page_amount + 1; p++){
-        $.getJSON(path + "&page=" + p, function(data){
-          if(data.length == 0){
-            return;
-          }
-          for(var i = 0; i < data.length; i++){
-            task_state = data[i].content.rendered.replace(/(<([^>]+)>)/ig,"").replace(/\s+/g, "");
-            checked_str = "false";
-            if(task_state.match(/done/)){
-              checked_str = "true"
-            }
-            // li を append
-            table.append('<tr data-my_task_id="' + data[i].id + '" data-task_checked="' + checked_str + '" data-task_id="' + data[i].title.rendered + '"></tr>');
-
-            // ID を用いてタスクプールから検索
-            $.getJSON("<?php echo home_url('/');?>wp-json/wp/v2/task/" + data[i].title.rendered + "?_embed", function(item){
-              // サムネイルの取得
-              $(item._embedded['wp:featuredmedia']).each(function(index, element){
-                media_url = element.source_url;
-              });
-              var is_checked = ""
-              if($('tr[data-task_id="' + item.id + '"]').data('task_checked')){
-                is_checked = "checked";
-              }
-              // li の中身を append
-              $('tr[data-task_id="' + item.id + '"]').append(
-                '<td><img src="' + media_url + '" width="50"></td>' + 
-                '<td class="image-title">' + item.title.rendered + '</td>' +
-                '<td><input type="checkbox" data-task_id="' + item.id + '" class="checkbox" onclick="check_my_task($(this))"' + is_checked + '></td> ' +  
-                ''
-              );
-            });
-          }
-        });
-      }
-    });
-  }
-
-  // アクションフック
-  $(document).ready(function(){
-    load_task_table($('#task-table'));
-  });
+// アクションフック
+$(document).ready(function(){
+  load_task_table($('#task-table'));
+  hide_done_task();
+});
 </script>
 
 <?php get_footer(); ?>
