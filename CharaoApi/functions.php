d0f05a771a140513f80db1387fcf5641a05af71d
Ryosuke ONOSE <dev@onose.ws>
2016-11-05 09:36:48 +0900

    add my_task custom post type

--- CharaoApi/functions.php
+++ CharaoApi/functions.php
@@ -24,6 +24,38 @@ function create_post_type(){
       'show_ui' => true
     )
   );
+  // マイタスク (my_ask)
+  register_post_type('mt_task', array(
+    'labels' => array(
+      'name' => __('マイタスク'),
+      'singular_name' => __('マイタスク')
+    ),
+    'public' => true,
+    'menu_position' => 6,
+    'show_in_rest' => 'true'
+  ));
+  register_taxonomy(
+    'level', 'my_task', array(
+      'hierarchical' => false,
+      'update_count_callback' => '_update_post_term_count',
+      'label' => 'レベル',
+      'singular_label' => 'レベル',
+      'public' => true,
+      'show_ui' => true
+    )
+  );
+  register_taxonomy(
+    // true | false
+    'is_done', 'my_task', array(
+      'hierarchical' => false,
+      'update_count_callback' => '_update_post_term_count',
+      'label' => '終わった',
+      'singular_label' => '終わった',
+      'public' => true,
+      'show_ui' => true
+    )
+  );
+  
 }
 
 ?>
